ccaa0b068b07f888acd4d81823e71e1b1d366903
feiyueternal <feiyueternal@outlook.com>
2018-04-06 23:05:58 +0800

    ics_hdu_backend_utils/file_save.py  增加图片存储 templates/upload.html  上传图片网页模版

--- ICSHDU/settings.py
+++ ICSHDU/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -122,9 +122,12 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'stastic')
 STATIC_URL = '/static/'
 
+
 STATICFILES_DIRS = (
     ("css", os.path.join(STATIC_ROOT, 'css'))
     ("js", os.path.join(STATIC_ROOT, 'js'))
     ("images", os.path.join(STATIC_ROOT, 'images'))
     ("style", os.path.join(STATIC_ROOT, 'style'))
 )
+
+
